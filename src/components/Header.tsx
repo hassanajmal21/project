c43aa37c87a3b933acd7143c7b8b4ad26348503f
hassanajmal21 <hassan.32aug24webbpt@gmail.com>
2025-07-17 22:50:06 +0500

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link to="/" className="text-2xl font-bold text-blue-600 hover:text-blue-700 transition-colors">
-         E-commerence
+        Dealyst
           </Link>
 
           {/* Search Bar */}
